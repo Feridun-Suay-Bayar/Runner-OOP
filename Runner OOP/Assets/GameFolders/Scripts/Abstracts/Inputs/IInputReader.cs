1db55de3c99b041f1ede084511b0a7b56ca181d6
Feridun Suay Bayar <61995157+Feridun-Suay-Bayar@users.noreply.github.com>
2023-03-29 00:54:59 +0300

    Horizontal Input
    Keyboard and joystick inputs has been done!

---
+++ Runner OOP/Assets/GameFolders/Scripts/Abstracts/Inputs/IInputReader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runner.Abstract.Inputs
+{
+    public interface IInputReader
+    {
+        float Horizontal { get; }
+    }
+}
+
--- Runner OOP/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ Runner OOP/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -1,7 +1,10 @@
+using Runner.Abstract.Inputs;
+using Runner.Inputs;
 using Runner.Movements;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Runner.Controllers
 {
@@ -15,16 +18,23 @@ namespace Runner.Controllers
         HorizontalMover _horizontalMover;
         JumpWithRigidbody _jumpWithRigidbody;
 
+        IInputReader _input;
+        float _horizontal;
         private void Awake()
         {
             _horizontalMover = new HorizontalMover(this);
             _jumpWithRigidbody= new JumpWithRigidbody(this);
+            _input = new InputReader(GetComponent<PlayerInput>());
+        }
+        private void Update()
+        {
+            _horizontal = _input.Horizontal;
         }
         private void FixedUpdate()
         {
             if (_horizontalMover != null)
             {
-                _horizontalMover.TickFixed(_horizantalDirection, _movementSpeed);
+                _horizontalMover.TickFixed(_horizontal, _movementSpeed);
             }
 
             if (_isJump)
